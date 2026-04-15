5de765c94f6c213a3ec76583d1da179ab2f58e4f
Yashwin <yashwinsrihari@gmail.com>
2026-04-16 01:37:54 +0530

    Add lint workflow

--- calculator.py
+++ calculator.py
@@ -1,5 +1,11 @@
+"""
+Simple calculator module
+"""
+
 def add(a, b):
+    """Return sum of two numbers"""
     return a + b
 
 def subtract(a, b):
+    """Return difference of two numbers"""
     return a - b
