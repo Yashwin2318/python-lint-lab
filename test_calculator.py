a58711e36298a549dd53c87c5240f2b8aa3caf60
Yashwin <yashwinsrihari@gmail.com>
2026-04-16 01:50:02 +0530

    Fix test

--- test_calculator.py
+++ test_calculator.py
@@ -1,7 +1,7 @@
 from calculator import add, subtract
 
 def test_add_positive_numbers():
-    assert add(2, 3) == 6
+    assert add(2, 3) == 5
 
 def test_add_negative_numbers():
     assert add(-2, -3) == -5
